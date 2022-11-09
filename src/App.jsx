23787e013becda2ce4e38a2815b0d0b47d04d297
nihalmuhmd <nihalozax@gmail.com>
2022-11-09 14:54:31 +0530

    Hii

--- src/App.jsx
+++ src/App.jsx
@@ -2,27 +2,27 @@ import React from "react";
 import "./styles.css";
 
 import Header from "./components/Header/Header";
-// import Nav from "./components/nav/nav";
+import Nav from "./components/nav/nav";
 import About from "./components/about/About";
-// import Experience from "./components/experience/Experience";
+import Experience from "./components/experience/Experience";
 // import Services from "./components/services/Services";
-// import Portfolio from "./components/portfolio/Portfolio";
-// import Testimonials from "./components/testimonials/Testimonials";
-// import Contact from "./components/contact/Contact";
-// import Footer from "./components/footer/Footer";
+import Portfolio from "./components/portfolio/Portfolio";
+import Testimonials from "./components/testimonials/Testimonials";
+import Contact from "./components/contact/Contact";
+import Footer from "./components/footer/Footer";
 
 export default function App() {
   return (
     <>
       <Header />
-      {/* <Nav /> */}
+      <Nav />
       <About />
-      {/* <Experience />
-      <Services />
+      <Experience />
+      {/* <Services /> */}
       <Portfolio />
       <Testimonials />
       <Contact />
-      <Footer /> */}
+      <Footer />
     </>
     // <div className="App">
     //   <h1>Hello CodeSandbox</h1>
